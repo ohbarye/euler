24285faa08b7825017088461afed38d3d27fa828
ohbarye <over.rye@gmail.com>
2016-07-23 11:20:48 +0900

    Solve problem 9 with ruby

---
+++ ruby/problem0009.rb
@@ -0,0 +1,12 @@
+# coding: utf-8
+
+# A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+#
+# a^2 + b^2 = c^2
+# For example, 3^2 + 4^2 = 9 + 16 = 25 = 5^2.
+#
+# There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+# Find the product abc.
+
+p [*1...1000].combination(2).map {|c| [*c, (Math.sqrt(c[0]**2 + c[1]**2))] }.find {|c| c.inject(:+) == 1000 }
+
